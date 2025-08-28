820c852ca319adc0cbfccc0f69db529367d91c7c
524hhhhhh <srose1225@naver.com>
2025-08-28 16:32:59 +0900

    ✨feat: 공통 컴포넌트 세팅

--- src/app/ui/common/combobox.tsx
+++ src/app/ui/common/combobox.tsx
@@ -17,68 +17,47 @@ import {
   PopoverTrigger,
 } from "@/app/ui/common/popover";
 
-type Status = {
+type ComboboxProps = { children: React.ReactNode };
+
+type ListType = {
+  id: string;
   value: string;
-  label: string;
 };
 
-const statuses: Status[] = [
-  {
-    value: "backlog",
-    label: "Backlog",
-  },
-  {
-    value: "todo",
-    label: "Todo",
-  },
-  {
-    value: "in progress",
-    label: "In Progress",
-  },
-  {
-    value: "done",
-    label: "Done",
-  },
-  {
-    value: "canceled",
-    label: "Canceled",
-  },
-];
+type Props = ComboboxProps & { items: ListType[] };
 
-export function ComboboxPopover() {
+export function ComboboxPopover({ children, items }: Props) {
   const [open, setOpen] = React.useState(false);
-  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
-    null,
-  );
+  const [selected, setSelected] = React.useState<ListType | null>(null);
 
   return (
     <div className="flex items-center space-x-4">
-      <p className="text-muted-foreground text-sm">Status</p>
+      <p className="text-muted-foreground text-sm">{children}</p>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="w-[150px] justify-start">
-            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          <Button variant="outline" className="w-auto justify-start">
+            {selected ? <>{selected.value}</> : <>+ {children} 선택</>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="p-0" side="right" align="start">
           <Command>
-            <CommandInput placeholder="Change status..." />
+            <CommandInput placeholder={`Change ${children}...`} />
             <CommandList>
               <CommandEmpty>No results found.</CommandEmpty>
               <CommandGroup>
-                {statuses.map((status) => (
+                {items.map((i) => (
                   <CommandItem
-                    key={status.value}
-                    value={status.value}
+                    key={i.id}
+                    value={i.value}
                     onSelect={(value) => {
-                      setSelectedStatus(
-                        statuses.find((priority) => priority.value === value) ||
+                      setSelected(
+                        items.find((priority) => priority.value === value) ||
                           null,
                       );
                       setOpen(false);
                     }}
                   >
-                    {status.label}
+                    {i.value}
                   </CommandItem>
                 ))}
               </CommandGroup>
