a9302f6d6bd52dfe83df93dd8730e1731666dd35
hanse <prsuw113@naver.com>
2025-08-30 15:21:49 +0900

    ♻️refactor: 디렉토리 구조 변환 & 시맨틱 태그 추가

--- src/app/types/commit.ts
+++ src/app/types/commit.ts
@@ -1,15 +1,6 @@
-interface CommitFile {
-  filename: string;
-  status: string;
-  patch: string | null;
-}
-
-interface CommitDetail {
-  sha: string;
-  author: string;
-  date: string;
+type CommitInfo = {
+  id: string;
   message: string;
-  files: CommitFile[] | null;
-}
+};
 
-export type { CommitFile, CommitDetail };
+export type { CommitInfo };
