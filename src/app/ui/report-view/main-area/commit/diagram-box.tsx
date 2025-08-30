cc33951063cf085f9ba181d76239e27e7fdf01ec
2hanse <129580349+2hanse@users.noreply.github.com>
2025-08-30 16:36:33 +0900

    Feat: Report 페이지 UI 작업 (#19)
    * ✨feat: 분석 페이지 UI 작업
    
    * 📦️chore: 목업 데이터 수정 및 디렉토리 이동
    
    * 📦️chore: 목업 데이터 경로 재설정
    
    * ♻️refactor: 폴더명 변경
    
    * ♻️refactor: 디렉토리 구조 변환 & 시맨틱 태그 추가

---
+++ src/app/ui/report-view/main-area/commit/diagram-box.tsx
@@ -0,0 +1,13 @@
+import mockdata from "@/mocks/data/report.json";
+
+function DiagramBox() {
+  return (
+    <div className="min-h-[200px] rounded-[10px] border-2 border-gray-500 p-2">
+      <p className="ml-2 text-xl font-bold">다이어그램</p>
+      <div className="border-[1px] border-gray-300"></div>
+      <p className="ml-2 text-lg">{mockdata.mockDiagram.diagramDescription}</p>
+    </div>
+  );
+}
+
+export default DiagramBox;
