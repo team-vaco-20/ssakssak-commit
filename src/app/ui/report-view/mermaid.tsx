edb62a5a4749ad3bb48120696796c81ec22ab8d7
codewith.mj / Minji Kim <88353455+codewith-MJ@users.noreply.github.com>
2025-08-31 12:20:49 +0900

    Feat: mermaid 적용 (#18)
    * ✨feat: mermaid 사용한 분석 데이터 시각화 컴포넌트 및 기능 추가
    
    * 📦️chore: 커스텀 훅 위치 hooks 폴더에서 해당 ui 컴포넌트 페이지로 변경
    
    * 🔨fix: default export 누락된 부분 추가

---
+++ src/app/ui/report-view/mermaid.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import parse from "html-react-parser";
+import useRenderMermaid from "@/app/ui/report-view/useRenderMermaid";
+import { MermaidProps } from "@/app/types/mermaid";
+
+export default function Mermaid({
+  chart,
+  title,
+  description,
+  config,
+}: MermaidProps) {
+  const { svg, error } = useRenderMermaid(chart, config);
+
+  if (error) {
+    return <div className="whitespace-pre-line text-amber-800">{error}</div>;
+  }
+
+  return (
+    <main className="w-full p-6">
+      <h1 className="mb-4 text-left text-xl font-semibold">{title}</h1>
+      <article className="space-y-4">
+        <p className="text-left">{description}</p>
+        <div className="flex justify-center">{parse(svg)}</div>
+      </article>
+    </main>
+  );
+}
