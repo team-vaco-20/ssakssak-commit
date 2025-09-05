5116050aa8160fa2631198108e618576b88e3355
2hanse <prsuw113@naver.com>
2025-09-05 17:30:00 +0900

    ✨feat: monaco-editor 라인 기반 자동 길이 조절 기능 추가

--- src/app/ui/report-view/monaco-file/MonacoFileViewer.tsx
+++ src/app/ui/report-view/monaco-file/MonacoFileViewer.tsx
@@ -4,6 +4,7 @@ import MonacoEditor from "@monaco-editor/react";
 import type { MonacoFileViewerProps } from "@/app/types/monaco";
 import { useMonacoEditorDecorations } from "@/hooks/useMonacoEditorDecorations";
 import { MonacoFileHeader } from "@/app/ui/report-view/monaco-file/MonacoFileHeader";
+import { MonacoAutoHeight } from "@/app/ui/report-view/monaco-file/MonacoAutoHeight";
 
 function MonacoFileViewer({
   filename,
@@ -23,24 +24,28 @@ function MonacoFileViewer({
     <div className="overflow-hidden rounded border border-gray-200">
       <MonacoFileHeader filename={filename} fileStatus={fileStatus} />
 
-      <MonacoEditor
-        height="400px"
-        language={language}
-        value={code}
-        options={{
-          readOnly: readOnly,
-          minimap: { enabled: true },
-          scrollBeyondLastLine: false,
-          wordWrap: "on",
-          fontSize: 13,
-          lineNumbers: "on",
-          glyphMargin: true,
-          folding: true,
-          renderWhitespace: "boundary",
-        }}
-        onMount={handleEditorMount}
-        theme="vs-light"
-      />
+      <MonacoAutoHeight code={code}>
+        {(height) => (
+          <MonacoEditor
+            height={height}
+            language={language}
+            value={code}
+            options={{
+              readOnly,
+              minimap: { enabled: false },
+              scrollBeyondLastLine: false,
+              wordWrap: "on",
+              fontSize: 13,
+              lineNumbers: "on",
+              glyphMargin: true,
+              folding: true,
+              renderWhitespace: "boundary",
+            }}
+            onMount={handleEditorMount}
+            theme="vs-light"
+          />
+        )}
+      </MonacoAutoHeight>
     </div>
   );
 }
