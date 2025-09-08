e375402f16d9be99960ce4b07393fd3476123570
524hhhhhh <srose1225@naver.com>
2025-09-08 23:34:33 +0900

    ✨feat: 특정 리포트 조회 기능 구현

---
+++ src/app/api/reports/[report-id]/route.ts
@@ -0,0 +1,34 @@
+import { AUTH_ERROR_MESSAGES } from "@/constants/error-messages";
+import { AppError, UnauthorizedError } from "@/errors";
+import authOptions from "@/lib/auth/auth-options";
+import { getByReportId } from "@/repositories/report";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+async function GET({ params }: { params: { reportId: string } }) {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user.userId;
+
+  if (!userId) {
+    throw new UnauthorizedError({ message: AUTH_ERROR_MESSAGES.UNAUTHORIZED });
+  }
+
+  try {
+    const report = await getByReportId(userId, params.reportId);
+
+    return NextResponse.json({ status: "ok", report }, { status: 200 });
+  } catch (error) {
+    const message: string =
+      error instanceof Error ? error.message : "Unexpected error";
+    const status: number = error instanceof AppError ? error.status : 500;
+
+    return NextResponse.json(
+      {
+        error: { message, status },
+      },
+      { status },
+    );
+  }
+}
+
+export { GET };
--- src/repositories/report.ts
+++ src/repositories/report.ts
@@ -1,20 +1,39 @@
 import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
 
 type DeleteManyArgs = {
   userId: string;
   reportIds: string[];
 };
 
+const reportCommonFields = Prisma.validator<Prisma.ReportSelect>()({
+  reportId: true,
+  reportTitle: true,
+  repositoryName: true,
+  owner: true,
+  branch: true,
+});
+
+async function getByReportId(userId: string, reportId: string) {
+  const report = await prisma.report.findFirst({
+    where: { reportId, userId },
+    select: {
+      ...reportCommonFields,
+      reportSummary: true,
+      reportConclusion: true,
+      commits: true,
+    },
+  });
+
+  return report;
+}
+
 async function getReports(userId: string) {
   return prisma.report.findMany({
     where: { userId, isActive: true },
     orderBy: { createdAt: "desc" },
     select: {
-      reportId: true,
-      reportTitle: true,
-      repositoryName: true,
-      owner: true,
-      branch: true,
+      ...reportCommonFields,
       createdAt: true,
     },
   });
@@ -29,4 +48,4 @@ async function deleteReports({ userId, reportIds }: DeleteManyArgs) {
   return result;
 }
 
-export { getReports, deleteReports };
+export { getByReportId, getReports, deleteReports };
