16e942d71cad6175708c9c818dfb8f8065e59234
524hhhhhh <srose1225@naver.com>
2025-09-09 17:16:43 +0900

    ♻️refactor: 불필요한 추상화 헤더 컴포넌트 제거

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,15 +1,10 @@
 import ReportForm from "@/app/ui/main/report-form";
-import Header from "@/app/ui/common/header";
+import LayoutHeader from "@/app/ui/layout/layout-header";
 
 function HomePage() {
   return (
     <div className="px-[10%]">
-      <Header
-        title="🧹싹싹커밋"
-        subTitlePrefix="복잡한 커밋을"
-        highlight="싹싹"
-        subTitleSuffix="모아서 한눈에 이해할 수 있게 도와드립니다"
-      />
+      <LayoutHeader />
 
       <ReportForm />
     </div>
