aee9a6be53dd82018862838ec486ba724e635189
codewith-MJ <minji.regina.kim@gmail.com>
2025-09-11 03:29:16 +0900

    ✨feat: Job 재시도 옵션 추가

--- src/infra/messaging/report-creation-queue.ts
+++ src/infra/messaging/report-creation-queue.ts
@@ -7,6 +7,8 @@ const reportCreationQueue = new Queue("reportCreation", { connection });
 
 const addJobs = async (data: unknown) => {
   return await reportCreationQueue.add("createReportJob", data, {
+    attempts: 3,
+    backoff: { type: "exponential", delay: 5000 },
     removeOnComplete: {
       age: 3600,
       count: 1000,
