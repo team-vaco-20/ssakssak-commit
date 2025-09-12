efeefa988d533c9fb306f2aad464d7b9ef9dd33e
524hhhhhh <srose1225@naver.com>
2025-09-12 21:41:59 +0900

    💄design: 서비스 설명 가독성 향상 위한 헤더 문구 수정

--- src/app/ui/layout/layout-header.tsx
+++ src/app/ui/layout/layout-header.tsx
@@ -3,11 +3,11 @@ function LayoutHeader() {
     <div className="flex flex-col items-center justify-center">
       <div className="py-2 pr-12 text-[40px] font-bold">🧹싹싹커밋</div>
       <div className="text-center">
-        <span className="text-gray-400">복잡한 커밋을 </span>
-        <span className="text-yellow-500">싹싹</span>
-        <span className="text-gray-400">
-          모아서 한 눈에 이해할 수 있게 도와드립니다
-        </span>
+        <span className="text-gray-400">복잡한 커밋을</span>
+        <span className="text-yellow-500"> 싹싹 </span>
+        <span className="text-gray-400">모아서 한 눈에 이해할 수 있게</span>
+        <span className="text-yellow-500"> 리포트</span>
+        <span className="text-gray-400">로 만들어 드립니다</span>
       </div>
     </div>
   );
